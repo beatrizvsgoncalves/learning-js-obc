19b134ca3bcf98605ef5cce3bfd4eefab30339cd
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-06-21 19:47:30 -0300

    Exercícios

---
+++ JS-II/aula-14/retorno.js
@@ -0,0 +1,19 @@
+function calcularMedia(a, b) {
+    const media = (a + b) / 2
+    return media 
+}
+
+const resultado = calcularMedia(7, 2)
+
+console.log(resultado)
+
+function criarProduto(nome, preco) {
+    const produto = {
+        nome, // nome: nome
+        preco, // preco: preco
+        estoque: 1
+    }
+    return produto
+}
+
+const notebook = criarProduto("Notebook Intel Core i3 8GB", 2500)
