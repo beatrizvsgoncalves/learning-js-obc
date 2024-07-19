cfc1384fcc09999deb94d9e9de4f61f2196fa011
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-07-19 18:53:33 -0300

    Exercícios

---
+++ JS-III/02-eventos/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+    console.log('mouse enter')
+}
+
+function onChange() {
+    console.log('change')
+}
+
+function onFocus() {
+    console.log('focus');
+}
+
+function onBlur() {
+    console.log('blur');
+}
+
+function onClick() {
+    console.log('click');
+}
