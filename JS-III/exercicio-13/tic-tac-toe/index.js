820a9e77475bd179afbb06a53bef59e82ba970b4
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-09-13 15:43:41 -0300

    Exercício

--- JS-III/exercicio-13/tic-tac-toe/index.js
+++ JS-III/exercicio-13/tic-tac-toe/index.js
@@ -1,67 +1,108 @@
-document.getElementById('button').addEventListener('click', function () {
-    const sectionPlayers = document.querySelector('section')
-    const player1Name = document.getElementById('player1').value
-    const player2Name = document.getElementById('player2').value
-    let playing = player1Name
+const boardRegions = document.querySelectorAll('#gameBoard div')
+let vBoard = []
+let turnPlayer = ''
 
-    let victoryCombinations = [
-        ['1', '2', '3'],
-        /* ['1', '3', '2'],
-        ['2', '1', '3'],
-        ['2', '3', '1'],
-        ['3', '1', '2'],
-        ['3', '2', '1'], */
-        ['4', '5', '6'],
-        ['7', '8', '9'],
-        ['1', '4', '7'],
-        ['2', '5', '8'],
-        ['3', '6', '9'],
-        ['1', '5', '9'],
-        ['3', '5', '7']
-    ]
-    let victory = undefined
+const buttonStart = document.getElementById('start')
+const msg = document.querySelector('p')
+const sectionPlayers = document.getElementById('players')
+
+function updateTitle() {
+    const playerInput = document.getElementById(turnPlayer)
+    document.getElementById('turnPlayer').innerText = playerInput.value
+}
+
+function updateMenu() {
+    sectionPlayers.style.display === 'none' ? sectionPlayers.style.display = 'flex' : sectionPlayers.style.display = 'none'
+    buttonStart.style.display === 'none' ? buttonStart.style.display = 'block' : buttonStart.style.display = 'none', buttonStart.innerText = 'Reiniciar'
+}
+
+function initializeGame() {
+    vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+    turnPlayer = 'player1'
     
-    if (player1Name !== '' && player2Name !== '') {
-        do {
-            sectionPlayers.innerText = player1Name
-            sectionPlayers.style.fontSize = '2.2rem'
-            sectionPlayers.style.textTransform = 'uppercase'
-            sectionPlayers.style.transform = '2s'
-            let player1Combination = []
-            let player2Combination = []
-            
-            
-            document.querySelectorAll('.box').forEach(function (box) {
-                box.addEventListener('click', function () {
-                    if (playing === player1Name) {
-                        box.classList.add('x')
-                        playing = player2Name
-                        sectionPlayers.innerText = player2Name
-                        player1Combination.push(box.dataset.value)
-                        
-                        console.log(player1Combination)
-                    } else {
-                        box.classList.add('circle')
-                        playing = player1Name
-                        sectionPlayers.innerText = player1Name
-                        player2Combination.push(box.dataset.value)
+    updateMenu()
+    msg.style.display = 'block'
+    msg.innerHTML = 'Vez de <span id="turnPlayer"></span>'
+    updateTitle()
+    
+    boardRegions.forEach(function (element) {
+        element.classList.remove('win')
+        element.classList.remove('x')
+        element.classList.remove('circle')
+        element.classList.add('cursor-pointer')
+        element.addEventListener('click', handleBoardClick)
+    })
+}
 
-                        console.log(player2Combination)
-                    }
-                })
-            })
+function getWinRegions() {
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+        winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+        winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+        winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+        winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+        winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+        winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+        winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+        winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+}
 
-            let result = victoryCombinations.forEach(function (array) {
-                array.includes(player1Combination)
-            })
+function disableRegion(element) {
+    element.classList.remove('cursor-pointer')
+    element.removeEventListener('click', handleBoardClick)
+}
 
-            console.log(result);
-            
-            if (result) {
-                
-            }
-        } while (victory === undefined)
+function handleWin(regions) {
+    regions.forEach(function (region) {
+      document.querySelector('[data-region="' + region + '"]').classList.add('win')
+    })
+    const playerName = document.getElementById(turnPlayer).value
+    msg.innerHTML = playerName + ' venceu!'
+    updateMenu()
+    boardRegions.forEach(function (element) {
+        element.classList.remove('cursor-pointer')
+        element.removeEventListener('click', handleBoardClick)
+    })
+}
+
+function handleBoardClick(ev) {
+    const div = ev.currentTarget
+    const region = div.dataset.region
+    const rowColumnPair = region.split('.')
+    const row = rowColumnPair[0]
+    const column = rowColumnPair[1]
+
+    if (turnPlayer === 'player1') {
+        div.classList.add('x')
+        vBoard[row][column] = 'X'
     } else {
-        alert('Preencha os campos!')
+        div.classList.add('circle')
+        vBoard[row][column] = 'O'
     }
-})
+
+    console.clear()
+    console.table(vBoard)
+
+    disableRegion(div)
+
+    const winRegions = getWinRegions()
+    if (winRegions.length > 0) {
+        handleWin(winRegions)
+    } else if (vBoard.flat().includes('')) {
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+        updateTitle()
+    } else {
+        msg.innerHTML = 'Empate!'
+        updateMenu()
+    }
+}
+
+document.getElementById('start').addEventListener('click', initializeGame)
