8004cdd130aa352fed9de6102cc5e7d0016d1173
Bowie <beatrizvsgoncalves.274@gmail.com>
2024-09-16 19:41:50 -0300

    Exercícios

---
+++ JS-III/24-armazenamento/index.js
@@ -0,0 +1,48 @@
+// Session Storage
+
+document.getElementById("sessionBtn").addEventListener("click", function () {
+	const input = document.getElementById("session");
+	sessionStorage.setItem("info", input.value);
+	input.value = "";
+});
+
+document.getElementById("readSession").addEventListener("click", function () {
+	const info = sessionStorage.getItem("info");
+	alert(`A informação salva na session storage é: '${info}'`);
+});
+
+// Local Storage
+
+document.getElementById("localBtn").addEventListener("click", function () {
+	const input = document.getElementById("local");
+	localStorage.setItem("text", input.value);
+	input.value = "";
+});
+
+document.getElementById("readLocal").addEventListener("click", function () {
+	const text = localStorage.getItem("text");
+	alert(`O texto salvo no local storage é: '${text}'`);
+});
+
+// Cookies
+
+document.getElementById("cookieBtn").addEventListener("click", function () {
+	const input = document.getElementById("cookie");
+	// cookieName=value; expires=UTCStringDate; path=/
+	const cookie = `info=${input.value};`;
+	const expiration = `expires=${new Date(2024, 8, 17)};`;
+	const path = "path=/;";
+	document.cookie = cookie + expiration + path;
+	input.value = "";
+	console.log(document.cookie);
+});
+
+document.getElementById("cookie2Btn").addEventListener("click", function () {
+	const input = document.getElementById("cookie2");
+	const cookie = `text=${input.value};`;
+	const expiration = `expires=${new Date(2024, 8, 17)};`;
+	const path = "path=/;";
+	document.cookie = cookie + expiration + path;
+	input.value = "";
+	console.log(document.cookie);
+});
