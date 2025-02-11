524731d55e817c748c7f592579b03801d7a13196
Bowie <beatrizvsgoncalves.274@gmail.com>
2025-02-11 14:35:44 -0300

    Exercício sobre o webpack

--- JS-IV/32-usando-webpack/webpack.config.js
+++ JS-IV/32-usando-webpack/webpack.config.js
@@ -1,13 +1,17 @@
-const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
-        main: './src/index.js',
-        hello: './src/hello.js'
+        index: './src/index.js'
     },
     mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.min.js'
-    }
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 }
