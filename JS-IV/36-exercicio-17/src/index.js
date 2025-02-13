eca13f38bc6f6e312e6e848587a789dc107a9eb2
Bowie <beatrizvsgoncalves.274@gmail.com>
2025-02-13 16:30:21 -0300

    Ajustes finais

---
+++ JS-IV/36-exercicio-17/src/index.js
@@ -0,0 +1,6 @@
+import './styles/index.css'
+
+const today = new Date()
+const hello = () => console.log(`It works! Today is ${today.getDay()}!}`) 
+
+hello()
--- JS-IV/36-exercicio-17/webpack.config.js
+++ JS-IV/36-exercicio-17/webpack.config.js
@@ -1,34 +1,21 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
-
 module.exports = {
-    entry: {
-        build: './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].min.js',
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin()
+  entry: {
+    index: './src/index.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+      },
+      {
+          test: /\.js$/,
+          use: 'babel-loader'
+      }
     ]
+  },
+  output: {
+      filename: '[name].min.js',
+  }
 }
