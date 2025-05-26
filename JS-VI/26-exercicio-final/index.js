0662cdb126094141373395afc1c4803b31c9c7d9
Beatriz Gonçalves <125322204+beatrizvsgoncalves@users.noreply.github.com>
2025-05-26 18:32:52 -0300

    Exercício Final - Consumindo uma API [resolução do professor]

--- JS-VI/26-exercicio-final/index.js
+++ JS-VI/26-exercicio-final/index.js
@@ -24,24 +24,90 @@ function createTransactionAmount(amount) {
 	});
 	const formattedAmount = formater.format(amount);
 
-	if (amount < 0) {
+	if (amount > 0) {
 		span.textContent = `${formattedAmount} C`;
-		span.classList.add("Credit");
+		span.classList.add("transaction-amount", "credit");
 	} else {
 		span.textContent = `${formattedAmount} D`;
-		span.classList.add("Debit");
+		span.classList.add("transaction-amount", "debit");
 	}
 
 	return span;
+}
+
+function createEditTransactionButton(transaction) {
+	const editBtn = document.createElement("button");
+	editBtn.classList.add("edit-btn");
+	editBtn.textContent = "Editar";
+	editBtn.addEventListener("click", () => {
+		document.querySelector("#id").value = transaction.id;
+		document.querySelector("#name").value = transaction.name;
+		document.querySelector("#amount").value = transaction.amount;
+	});
+	return editBtn;
+}
+
+function createDeleteTransactionButton(id) {
+	const deleteBtn = document.createElement("button")
+	deleteBtn.classList.add("delete-btn");
+	deleteBtn.textContent = "Excluir";
+	deleteBtn.addEventListener("click", async () => {
+		await fetch(`http://localhost:3000/transactions/${id}`, { method: "DELETE" });
+		deleteBtn.parentElement.remove();
+		const indexToRemove = transactions.findIndex((t) => t.id === id);
+		transactions.splice(indexToRemove, 1);
+		updateBalance();
+	});
+	return deleteBtn;
 }
 
 function renderTransaction(transaction) {
 	const container = createTransactionContainer(transaction.id);
 	const title = createTransactionTitle(transaction.name);
 	const amount = createTransactionAmount(transaction.amount);
+	const editBtn = createEditTransactionButton(transaction);
+	const deleteBtn = createDeleteTransactionButton(transaction.id);
 
-	container.append(title, amount);
+	container.append(title, amount, editBtn, deleteBtn);
 	document.querySelector("#transactions").append(container);
+}
+
+async function saveTransaction(ev) {
+	ev.preventDefault();
+
+	const id = document.querySelector("#id").value;
+	const name = document.querySelector("#name").value;
+	const amount = parseFloat(document.querySelector("#amount").value);
+
+	if (id) {
+		const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, amount })
+		})
+		const transaction = await response.json();
+		const indexToRemove = transactions.findIndex((t) => t.id === id)
+		transactions.splice(indexToRemove, 1, transaction);
+		document.querySelector(`#transaction-${id}`).remove();
+		renderTransaction(transaction);
+	} else {
+		const response = await fetch("http://localhost:3000/transactions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, amount })
+		});
+	
+		const transaction = await response.json();
+		transactions.push(transaction);
+		renderTransaction(transaction);
+	}
+
+	ev.target.reset();
+	updateBalance();
 }
 
 async function fetchTransactions() {
@@ -51,21 +117,25 @@ async function fetchTransactions() {
 }
 
 function updateBalance() {
-  const balanceSpan = document.querySelector("#balance");
-  const balance = transactions.reduce((sum, transactions) => sum + transactions.amount, 0);
-  const formater = Intl.NumberFormat("pt-BR", {
-    compactDisplay: "long",
-    currency: "BRL",
-    style: "currency",
-  });
-  balanceSpan.textContent = formater.format(balance);
+	const balanceSpan = document.querySelector("#balance");
+	const balance = transactions.reduce(
+		(sum, transactions) => sum + transactions.amount,
+		0
+	);
+	const formater = Intl.NumberFormat("pt-BR", {
+		compactDisplay: "long",
+		currency: "BRL",
+		style: "currency",
+	});
+	balanceSpan.textContent = formater.format(balance);
 }
 
 async function setup() {
 	const results = await fetchTransactions();
 	transactions.push(...results);
 	transactions.forEach(renderTransaction);
-  updateBalance();
+	updateBalance();
 }
 
 document.addEventListener("DOMContentLoaded", setup);
+document.querySelector("form").addEventListener("submit", saveTransaction);
